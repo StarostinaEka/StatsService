b2e0094d921e26a34e8e9b2c6a8c5baad0471dfc
Eka Starostina <starostinaeka@gmail.com>
2023-02-11 13:25:34 +0300

    First commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -12,5 +12,59 @@ public class StatsService {
 
     public long averageSale(long[] sales) {
 
+        return sumSales(sales) / sales.length;
+    }
+
+
+    public int monthMaxSale(long[] sales) {
+        int maxMonth = 0;
+        long maxSale = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= maxSale) {
+                maxMonth = i;
+                maxSale = sales[i];
+            }
+
+        }
+        return  maxMonth + 1;
+    }
+
+    public int monthMinSale(long[] sales) {
+        int minMonth = 0;
+        long minSale = sales[0];
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= minSale) {
+                minMonth = i;
+                minSale = sales[i];
+            }
+
+        }
+        return  minMonth + 1;
+    }
+
+    public int monthsBelowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < averageSale(sales)) {
+                counter++;
+
+            }
+
+        }
+        return counter;
+    }
+
+    public int monthsAboveAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > averageSale(sales)) {
+                counter++;
+
+            }
+
+        }
+        return counter;
     }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -14,5 +14,56 @@ class StatsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
 
+    public void testAverageSale() {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long actual = service.averageSale(sales);
+        long expected = (8+ 15+ 13+ 15+ 17+ 20+ 19+ 20+ 7+ 14+ 14+ 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testMonthMaxSale() {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long actual = service.monthMaxSale(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testMonthMinSale() {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long actual = service.monthMinSale(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+
+    public void testMonthsBelowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long actual = service.monthsBelowAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+
+    public void testMonthsAboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+        long actual = service.monthsAboveAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
